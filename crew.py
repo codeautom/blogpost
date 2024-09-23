3231414f9e51e0bf6f249144ebbc0e4f665b87b8
sohoml <haider@sohoby.sa>
2024-09-23 19:30:34 +0500

    markdown

--- crew.py
+++ crew.py
@@ -1,9 +1,7 @@
-
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import sqlite3
-
 
 import streamlit as st
 from crewai import Crew
@@ -14,11 +12,22 @@ from tasks import plan, write, edit
 def main():
     st.title("Multi-Agent Workflow with CrewAI and Groq")
 
+    # Display introductory text
+    st.markdown("""
+    **Multi-agent workflows in content creation** involve specialized roles, 
+    such as planners, writers, and editors, working together to enhance efficiency, 
+    creativity, and quality. These systems allow for parallel task execution, minimizing 
+    bottlenecks and fostering collaboration. Platforms like CrewAI enable coordination among 
+    agents, while Groq’s advanced language models like Llama3–70B support content generation.
+     The process includes content planning, writing, and editing to ensure polished, engaging, 
+     and accurate output. This approach offers scalability to meet growing content demands.
+    """)
+
     # Text input for the topic
     topic = st.text_input("Enter a topic for the AI crew", "")
 
-    if st.button("Genrate blog"):
-        with st.spinner('Runing..'):
+    if st.button("Generate blog"):
+        with st.spinner('Running..'):
             try:
                 # Initialize the crew
                 crew = Crew(
